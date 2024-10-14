759e5f92b94b2f12757f80ab3b0f098c1c76fa97
douglas-sola <v671408@gmail.com>
2024-10-13 23:21:37 -0300

    sla de novo

--- aula6/aula6.cs
+++ aula6/aula6.cs
@@ -5,5 +5,21 @@ class Principal
     static void Main()
     {
         
+        int a , b , c;
+        a = 10;
+        b = 20;
+        c = 30;
+        Console.WriteLine("\na\t = {0}\n \nb\t = {1}\n \nc\t = {2}\n",a,b,c);
+        int dc = 1;
+        double valorcompra = 5;
+        double valorvenda;
+        double lucro = 0.90;
+        string produto = "Manga";
+        valorvenda = valorcompra + (valorcompra * lucro);
+        int egshg = 6576; //inutio
+        Console.WriteLine("Produto.............{0, 15}", produto);
+        Console.WriteLine("val.compra..........{0, 15 : c}", valorcompra);
+        Console.WriteLine("lucro...............{0, 15:p}", lucro);
+        Console.WriteLine("val.venda...........{0, 15:c}", valorvenda);
     }
 }
