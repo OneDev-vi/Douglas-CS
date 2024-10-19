f36482d377696123a75783e0350933c454baed30
douglas-sola <v671408@gmail.com>
2024-10-19 19:23:55 -0300

    arrys normais kkk ate que é facinho kkkkkk.

---
+++ aula17/aula17.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Principal
+{
+    static void Main(String[] args)
+    {
+        int a, b, c, d;
+        /*
+        new[] n = new int[5];
+        //global::System.Object value = n[1,2,3,4,5];
+        n[0] = 12;
+        n[1] = 24;
+        n[2] = 48;
+        n[3] = 96;
+        n[4] = 19;
+        Console.WriteLine(n[4]);
+        */
+        int[] num = new int[5]{1,2,3,4,5};
+        int[] n = {2,232,434,55};
+        Console.WriteLine(num[4]);
+    }
+}
--- aula2/obj/Debug/net8.0/aula2.AssemblyInfo.cs
+++ aula2/obj/Debug/net8.0/aula2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+325a89cf30746d3c64e031199debd267cc169b91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d881a6b1d4b4ffa686c8363cc21514626f3fb6a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
