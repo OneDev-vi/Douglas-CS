8d776ad798bc9fd7518a23ad1751dffc336305c2
douglas-sola <v671408@gmail.com>
2024-10-26 15:27:41 -0300

    basicamente isso praticando sempre a éééééééééééé

--- Pratica/5/5a.cs
+++ Pratica/5/5a.cs
@@ -5,7 +5,15 @@ class Principal
     enum dias {segunda, terça, quarta, quinta, sexta, sabado, domingo};
     static void Main()
     {
-        dias ds = dias.segunda;
+        dias ds = dias.segunda; // 1
+        dias ds2 = (dias)2; // 2
+        dias sa = (dias)5;
+        int se = (int)dias.quinta;
         Console.WriteLine(ds);
+        Console.WriteLine(ds2);
+        Console.WriteLine(sa);
+        Console.WriteLine(se);
+        
+        //Console.Clear();
     }
 }
